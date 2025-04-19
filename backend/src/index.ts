baa91bfd743e53d8cc4e39b1145774f397f82421
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2025-04-20 01:41:18 +0530

    Enhance server setup and package configuration for improved production handling and dependency management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
+import express from "express";
 import app from "./app";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+    });
+  }
   app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
