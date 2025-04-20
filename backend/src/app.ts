58550262baf9a8ebc5bbb12025c5afa0d43e481b
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2025-04-20 13:50:56 +0530

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import cors from "cors";
 import campaignRoutes from "./routes/campaign.routes";
 import messageRoutes from "./routes/message.routes";
 import linkedinRoutes from "./routes/linkedin.routes";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/campaigns", campaignRoutes);
